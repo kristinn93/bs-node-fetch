b66fde16775bf6b5e36bdb45fe76183f88decdfb
kristinn93 <kristinnjuliusson@gmail.com>
2018-10-10 19:50:11 +0000

    Fix for es6 import

--- src/bs_node_fetch.mli
+++ src/bs_node_fetch.mli
@@ -212,7 +212,7 @@ module Response : sig
   external text : string Js.Promise.t = "" [@@bs.send.pipe: t]
 end
 
-external fetch : string -> response Js.Promise.t = "node-fetch" [@@bs.module]
-external fetchWithInit : string -> requestInit -> response Js.Promise.t = "node-fetch" [@@bs.module]
-external fetchWithRequest : request -> response Js.Promise.t = "node-fetch" [@@bs.module]
-external fetchWithRequestInit : request -> requestInit -> response Js.Promise.t = "node-fetch" [@@bs.module]
+external fetch : string -> response Js.Promise.t = "default" [@@bs.module "node-fetch"]
+external fetchWithInit : string -> requestInit -> response Js.Promise.t = "default" [@@bs.module "node-fetch"]
+external fetchWithRequest : request -> response Js.Promise.t = "default" [@@bs.module "node-fetch"]
+external fetchWithRequestInit : request -> requestInit -> response Js.Promise.t = "default" [@@bs.module "node-fetch"]
